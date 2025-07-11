c82c2f61708ff5cb2d3f491f4c82a0c79c8824f9
bassemhoteit1 <82917114+bassemhoteit1@users.noreply.github.com>
2025-07-11 14:49:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ const closeBtn = document.getElementById("close-btn");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.add("active");
 });
-
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
+});
+
+AOS.init();
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: { el: '.swiper-pagination', clickable: true },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
